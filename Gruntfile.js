96fa60cd2d94f4a5d9a0820bbe06178f346efbac
Curtis Oakley <coakley@chocklydigital.com>
2015-03-10 10:07:13 -0600

    Add uglify.js to minimize the console helper for distribution.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,27 @@
 /**
- * Created by coakley on 3/9/15.
+ * McMyAdminHelper
+ * http://chockly.org/
+ *
+ * Copyright © 2015 Curtis Oakley
+ * Licensed under the MIT license.
  */
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> v<%= pkg.version %> | (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %> | License: <%= pkg.license %> */',
+                sourceMap: true,
+                maxLineLen: 4096,
+                screwIE8: true
+            },
+            dist: {
+                files: {
+                    'dist/console-helper.min.js': ['src/console-helper.js']
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+};
