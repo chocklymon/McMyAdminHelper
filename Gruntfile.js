dc832150aa7f9bf9f9ca33c3101370de93af50a2
Curtis Oakley <coakley@chocklydigital.com>
2015-03-10 10:55:24 -0600

    Add eslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,23 +5,29 @@
  * Copyright © 2015 Curtis Oakley
  * Licensed under the MIT license.
  */
+/*global module*/
+"use strict";
 module.exports = function(grunt) {
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+        pkg: grunt.file.readJSON("package.json"),
         uglify: {
             options: {
-                banner: '/*! <%= pkg.name %> v<%= pkg.version %> | (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %> | License: <%= pkg.license %> */',
+                banner: "/*! <%= pkg.name %> v<%= pkg.version %> | (c) <%= grunt.template.today('yyyy') %> <%= pkg.author %> | License: <%= pkg.license %> */",
                 sourceMap: true,
                 maxLineLen: 4096,
                 screwIE8: true
             },
             dist: {
                 files: {
-                    'dist/console-helper.min.js': ['src/console-helper.js']
+                    "dist/console-helper.min.js": ["src/console-helper.js"]
                 }
             }
+        },
+        eslint: {
+            target: ["src/**/*.js", "Gruntfile.js"]
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-eslint");
 };
