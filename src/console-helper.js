b4bb144ba5133fa454818e717a31faf62ab6f4da
Curtis Oakley <coakley@chocklydigital.com>
2016-12-15 09:17:54 -0700

    Ignore html entities when colorizing messages.

--- src/console-helper.js
+++ src/console-helper.js
@@ -461,6 +461,122 @@ var ch = {
         }
     },
 
+    colorize: function (text) {
+        var regex = /(&[0-9a-fk-or])[^;]{3}/g,
+            result,
+            style,
+            styleTag,
+            resetCount = 0;
+
+        while ((result = regex.exec(text)) != null) {
+            switch (result[1]) {
+                case "&0":
+                    style = "black";
+                    break;
+
+                case "&1":
+                    style = "dark-blue";
+                    break;
+
+                case "&2":
+                    style = "dark-green";
+                    break;
+
+                case "&3":
+                    style = "dark-aqua";
+                    break;
+
+                case "&4":
+                    style = "dark-red";
+                    break;
+
+                case "&5":
+                    style = "dark-purple";
+                    break;
+
+                case "&6":
+                    style = "gold";
+                    break;
+
+                case "&7":
+                    style = "gray";
+                    break;
+
+                case "&8":
+                    style = "dark-gray";
+                    break;
+
+                case "&9":
+                    style = "blue";
+                    break;
+
+                case "&a":
+                    style = "green";
+                    break;
+
+                case "&b":
+                    style = "aqua";
+                    break;
+
+                case "&c":
+                    style = "red";
+                    break;
+
+                case "&d":
+                    style = "light-purple";
+                    break;
+
+                case "&e":
+                    style = "yellow";
+                    break;
+
+                case "&f":
+                    style = "white";
+                    break;
+
+                case "&k":
+                    style = "obfuscated";
+                    break;
+                case "&l":
+                    style = "bold";
+                    break;
+
+                case "&m":
+                    style = "strikethrough";
+                    break;
+
+                case "&n":
+                    style = "underline";
+                    break;
+
+                case "&o":
+                    style = "italic";
+                    break;
+
+                case "&r":
+                    style = false;
+                    break;
+            }
+            if (style === false) {
+                // Reset
+                styleTag = "";
+                for (; resetCount > 0; resetCount--) {
+                    styleTag += "</span>";
+                }
+            } else {
+                styleTag = '<span class="ch-' + style + '">';
+                resetCount++;
+            }
+            text = text.substring(0, result.index) + styleTag + "<small>§" + result[1][1] + "</small>" + text.substring(result.index + 2);
+        }
+
+        // Close any open style tags
+        for (; resetCount > 0; resetCount--) {
+            text += "</span>";
+        }
+        return text;
+    },
+
     /**
      * Process a chat message for input into a row.
      * @param {string} text The message to process
@@ -469,11 +585,7 @@ var ch = {
     processMessage: function (text) {
         var filters = DataStorage.get(DataStorage.key.filters, []),
             filter,
-            regex,
-            result,
-            style,
-            styleTag,
-            resetCount = 0;
+            regex;
 
         // Escape any HTML entities
         text = text.replace(/</g, "&lt;").replace(/>/g, "&gt;");
@@ -506,113 +618,7 @@ var ch = {
         }
 
         if (DataStorage.get(DataStorage.key.colorize, false)) {
-            regex = /&[0-9a-fk-or]/g;
-            while ((result = regex.exec(text)) != null) {
-                switch (result[0]) {
-                    case "&0":
-                        style = "black";
-                        break;
-
-                    case "&1":
-                        style = "dark-blue";
-                        break;
-
-                    case "&2":
-                        style = "dark-green";
-                        break;
-
-                    case "&3":
-                        style = "dark-aqua";
-                        break;
-
-                    case "&4":
-                        style = "dark-red";
-                        break;
-
-                    case "&5":
-                        style = "dark-purple";
-                        break;
-
-                    case "&6":
-                        style = "gold";
-                        break;
-
-                    case "&7":
-                        style = "gray";
-                        break;
-
-                    case "&8":
-                        style = "dark-gray";
-                        break;
-
-                    case "&9":
-                        style = "blue";
-                        break;
-
-                    case "&a":
-                        style = "green";
-                        break;
-
-                    case "&b":
-                        style = "aqua";
-                        break;
-
-                    case "&c":
-                        style = "red";
-                        break;
-
-                    case "&d":
-                        style = "light-purple";
-                        break;
-
-                    case "&e":
-                        style = "yellow";
-                        break;
-
-                    case "&f":
-                        style = "white";
-                        break;
-
-                    case "&k":
-                        style = "obfuscated";
-                        break;
-                    case "&l":
-                        style = "bold";
-                        break;
-
-                    case "&m":
-                        style = "strikethrough";
-                        break;
-
-                    case "&n":
-                        style = "underline";
-                        break;
-
-                    case "&o":
-                        style = "italic";
-                        break;
-
-                    case "&r":
-                        style = false;
-                        break;
-                }
-                if (style === false) {
-                    // Reset
-                    styleTag = "";
-                    for (; resetCount > 0; resetCount--) {
-                        styleTag += "</span>";
-                    }
-                } else {
-                    styleTag = '<span class="ch-' + style + '">';
-                    resetCount++;
-                }
-                text = text.substring(0, result.index) + styleTag + "§" + result[0][1] + text.substring(result.index + 2);
-            }
-
-            // Close any open style tags
-            for (; resetCount > 0; resetCount--) {
-                text += "</span>";
-            }
+            text = ch.colorize(text);
         }
 
         return text;
